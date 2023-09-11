7e1be9c0de6127b0fe1c3b6963af3c57eb6f7043
yourlowkey <m.phuc2552000@gmail.com>
2023-09-12 01:53:12 +0700

    dark mode with redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { themeSettings } from "theme";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
